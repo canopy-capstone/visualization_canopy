93f2d800f69d89f61cb5362912b1cef68842a491
uubm <wbmanning30@outlook.com>
2023-12-12 17:34:10 -0700

    sort of working color picker

--- mesh-visualizer/src/App.js
+++ mesh-visualizer/src/App.js
@@ -1,71 +1,55 @@
-import { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 import '@kitware/vtk.js/Rendering/Profiles/Geometry';
 
 import vtkFullScreenRenderWindow from '@kitware/vtk.js/Rendering/Misc/FullScreenRenderWindow';
 
-import vtkActor           from '@kitware/vtk.js/Rendering/Core/Actor';
-import vtkMapper          from '@kitware/vtk.js/Rendering/Core/Mapper';
-// import vtkOBJReader from "@kitware/vtk.js/IO/Misc/OBJReader";
-import vtkSTLReader from "@kitware/vtk.js/IO/Geometry/STLReader";
-// import vtkColorTransferFunction from '@kitware/vtk.js/Rendering/Core/ColorTransferFunction';
+import vtkActor from '@kitware/vtk.js/Rendering/Core/Actor';
+import vtkMapper from '@kitware/vtk.js/Rendering/Core/Mapper';
+import vtkSTLReader from '@kitware/vtk.js/IO/Geometry/STLReader';
 import vtkDataArray from '@kitware/vtk.js/Common/Core/DataArray';
 
 // Function to load the thickness text file
 function readThicknessValuesFromFile(url) {
   return fetch(url)
-    .then(response => response.text())
-    .then(data => data.trim().split('\n').map(Number))
-    .catch(error => {
+    .then((response) => response.text())
+    .then((data) => data.trim().split('\n').map(Number))
+    .catch((error) => {
       console.error('Error reading the file:', error);
       return [];
     });
 }
 
 // This function will load the coloring for the triangles based on their thickness
-function load_gradient(thickness, min, max, transparency) {
+function load_gradient(thickness, min, max, transparency, startColor, endColor) {
   const color_arr = new Uint8Array(thickness.length * 4);
   let normalizedVal, curr_thickness;
 
-  // const r = Math.abs(rgb_max[0] - rgb_min[0]);
-  // const g = Math.abs(rgb_max[1] - rgb_min[1]);
-  // const b = Math.abs(rgb_max[2] - rgb_min[2]);
   // Iterate through every value in the thickness file and assign a color corresponding to it
   for (let i = 0; i < color_arr.length; i += 4) {
     curr_thickness = thickness[i / 4];
 
     // Apply logarithmic transformation
-    curr_thickness = Math.log(curr_thickness + 1);  // Adding 1 to avoid log(0)
+    curr_thickness = Math.log(curr_thickness + 1); // Adding 1 to avoid log(0)
     normalizedVal = (curr_thickness - Math.log(min + 1)) / (Math.log(max + 1) - Math.log(min + 1));
 
-    color_arr[i] = Math.round(255 * (normalizedVal));
-    color_arr[i+1] = 0;
-    color_arr[i+2] = Math.round(255 * (1 - normalizedVal));
+    color_arr[i] = Math.round(startColor.r + (endColor.r - startColor.r) * normalizedVal);
+    color_arr[i + 1] = Math.round(startColor.g + (endColor.g - startColor.g) * normalizedVal);
+    color_arr[i + 2] = Math.round(startColor.b + (endColor.b - startColor.b) * normalizedVal);
     color_arr[i + 3] = normalizedVal <= transparency ? 255 : 0;
   }
 
   return color_arr;
 }
 
-function hexToRgb(hex) {
-  var bigint = parseInt(hex, 16);
-  var r = (bigint >> 16) & 255;
-  var g = (bigint >> 8) & 255;
-  var b = bigint & 255;
-
-  return [r, g, b];
-}
-
 let minThickness, maxThickness, thicknessValues;
-const rgbToHex = (rgb) => `#${rgb.map((val) => val.toString(16).padStart(2, '0')).join('')}`;
-
 function App() {
   const vtkContainerRef = useRef(null);
   const [transparency, setTransparency] = useState(1.0);
   const [initialized, setInitialized] = useState(false);
+  const [startColor, setStartColor] = useState({ r: 0, g: 0, b: 255 });
+  const [endColor, setEndColor] = useState({ r: 0, g: 0, b: 255 });
   const fullScreenRendererRef = useRef(null);
-  const [rgb_min, setRgb_min] = useState([255, 0, 0]);
-  const [rgb_max, setRgb_max] = useState([0, 0, 255]);
 
   // Load STL and initialize thickness values on component mount
   useEffect(() => {
@@ -94,7 +78,14 @@ function App() {
         maxThickness = Math.max(...thicknessValues);
 
         // Initial color array
-        const colors = load_gradient(thicknessValues, minThickness, maxThickness, transparency);
+        const colors = load_gradient(
+          thicknessValues,
+          minThickness,
+          maxThickness,
+          transparency,
+          startColor,
+          endColor
+        );
         const colorDataArray = vtkDataArray.newInstance({
           name: 'Colors',
           values: colors,
@@ -106,66 +97,47 @@ function App() {
         renderer.addActor(actor);
         renderer.resetCamera();
         fullScreenRendererRef.current.getRenderWindow().render();
-        setInitialized(true)
+        setInitialized(true);
       } catch (error) {
         console.error('Error loading STL:', error);
       }
     };
-    if(!initialized)
-    {
-      loadSTL();
-    }
-  },);
-  // [transparency, rgb_min, rgb_max]
+
+    loadSTL();
+  }, [startColor, endColor, transparency]);
 
   // Update color array when transparency changes
   useEffect(() => {
-    const loadColor = async () => {
-      if (initialized && fullScreenRendererRef.current) {
-        const renderer = fullScreenRendererRef.current.getRenderer();
-        const actor = renderer.getActors()[0]; // Assuming there is only one actor
+    if (initialized && fullScreenRendererRef.current) {
+      const renderer = fullScreenRendererRef.current.getRenderer();
+      const actor = renderer.getActors()[0]; // Assuming there is only one actor
 
-        if (actor) {
-          const colors = load_gradient(thicknessValues, minThickness, maxThickness, transparency);
-          const colorDataArray = vtkDataArray.newInstance({
-            name: 'Colors',
-            values: colors,
-            numberOfComponents: 4,
-          });
+      if (actor) {
+        const colors = load_gradient(
+          thicknessValues,
+          minThickness,
+          maxThickness,
+          transparency,
+          startColor,
+          endColor
+        );
+        const colorDataArray = vtkDataArray.newInstance({
+          name: 'Colors',
+          values: colors,
+          numberOfComponents: 4,
+        });
 
-          actor.getMapper().getInputData().getCellData().setScalars(colorDataArray);
-          console.log('Scalars Updated:', colorDataArray);
-
-          fullScreenRendererRef.current.getRenderWindow().render();
-        }
+        actor.getMapper().getInputData().getCellData().setScalars(colorDataArray);
+        fullScreenRendererRef.current.getRenderWindow().render();
       }
     }
-    loadColor();
-  }, [transparency, initialized]);
+  }, [transparency, initialized, startColor, endColor]);
 
-
-  // <input
-  //       type="color"
-  //       id="colorpicker"
-  //       value={rgbToHex(rgb_min)}
-  //       onChange={(e) => setRgb_min([...parseInt(e.target.value.slice(1, 3), 16), ...parseInt(e.target.value.slice(3, 5), 16), ...parseInt(e.target.value.slice(5), 16)])}
-  //       style={{ zIndex: 2, marginTop: '20px', left: '100px' }}
-  //     ></input>
-  //     <label for="colorpicker" style={{ zIndex: 2, marginTop: '20px' }}> To </label>
-  //     <input
-  //       type="color"
-  //       id="colorpicker"
-  //       value={rgbToHex(rgb_max)}
-  //       onChange={(e) => setRgb_max([...parseInt(e.target.value.slice(1, 3), 16), ...parseInt(e.target.value.slice(3, 5), 16), ...parseInt(e.target.value.slice(5), 16)])}
-  //       style={{ zIndex: 2, marginTop: '20px' }}
-  //     ></input>
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
       <div ref={vtkContainerRef} style={{ flex: 1 }}>
         {/* Adjust the flex property based on your layout */}
       </div>
-      
-
       <div
         style={{
           display: 'flex',
@@ -175,7 +147,9 @@ function App() {
           height: '100vh', // Make the container full height
         }}
       >
-        <label htmlFor="transparencySlider" style={{ zIndex: 2, marginBottom: '100px' }}>Min: {minThickness}</label>
+        <label htmlFor="transparencySlider" style={{ zIndex: 2, marginBottom: '100px' }}>
+          Min: {minThickness}
+        </label>
         <input
           style={{
             width: '400px',
@@ -185,18 +159,47 @@ function App() {
             marginBottom: '20px',
           }}
           type="range"
-          min='0'
-          max='1'
+          min="0"
+          max="1"
           step="0.01"
           value={transparency}
           onChange={(e) => setTransparency(parseFloat(e.target.value))}
         />
         {transparency}
-        <label htmlFor="transparencySlider" style={{ zIndex: 2, marginTop: '60px' }}>Max: {maxThickness}</label>
+        <td align="center" style={{ zIndex: 2, position: 'absolute', top: '10px' }}>
+          <label htmlFor="startColor">From:</label>
+          <input
+            type="color"
+            id="startColor"
+            value={`#${(startColor.r * 0x10000 + startColor.g * 0x100 + startColor.b)
+              .toString(16)
+              .padStart(6, '0')}`}
+            onChange={(e) => setStartColor(parseHexColor(e.target.value))}
+          />
+
+          <label htmlFor="endColor">To:</label>
+          <input
+            type="color"
+            id="endColor"
+            value={`#${(endColor.r * 0x10000 + endColor.g * 0x100 + endColor.b)
+              .toString(16)
+              .padStart(6, '0')}`}
+            onChange={(e) => setEndColor(parseHexColor(e.target.value))}
+          />
+        </td>
       </div>
-      
     </div>
   );
+}
+
+// Function to parse hex color to RGB
+function parseHexColor(hex) {
+  hex = hex.replace(/^#/, '');
+  const bigint = parseInt(hex, 16);
+  const r = (bigint >> 16) & 255;
+  const g = (bigint >> 8) & 255;
+  const b = bigint & 255;
+  return { r, g, b };
 }
 
 export default App;
